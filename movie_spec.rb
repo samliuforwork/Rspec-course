16c65b6a5abd5db27599edea9c87fa1392e43b7e
samliu <samliuforwork@gmail.com>
2021-02-23 18:06:31 +0800

    edit movies

--- movie_spec.rb
+++ movie_spec.rb
@@ -33,10 +33,26 @@ class Movie
     puts actor.act
     puts actor.fall_off_ladder
     puts actor.got_fire
+    puts actor.act
     end
   end
 end
 
-actor = Actor.new("James")
-movie = Movie.new(actor)
-movie.shooting
+# actor = Actor.new("James")
+# movie = Movie.new(actor)
+# movie.shooting
+
+RSpec.describe Movie do
+  let(:stuntman) { double("Mr.brown", ready?: true, act: "Dance", fall_off_ladder: "Help!", got_fire: true)}
+  subject { described_class.new(stuntman) }
+
+  describe 'start shooting method' do
+    it 'expect actor it do 3 action' do
+      expect(stuntman).to receive(:act).exactly(2).times
+      expect(stuntman).to receive(:ready?).exactly(1).times
+      expect(stuntman).to receive(:fall_off_ladder).exactly(1).times
+      expect(stuntman).to receive(:got_fire).exactly(1).times
+      subject.shooting
+    end
+  end
+end
